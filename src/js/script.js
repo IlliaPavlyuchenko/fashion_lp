4aaab14b80023eb450f1cc894b2b4cb869516b6a
IlliaPavlyuchenko <ilya.pavlyuchenko@gmail.com>
2026-01-29 17:31:24 +0200

    feat: add faqs section

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,23 @@
 AOS.init({ once: false, duration: 1500, offset: 180 });
 
 const body = document.querySelector('body');
+
+function accordion(accordionBlockSelector) {
+	const accordionTitleAll = document.querySelectorAll(accordionBlockSelector + ' [data-accordion-title]');
+
+	accordionTitleAll.forEach(function (item) {
+		item.addEventListener('click', function () {
+			if (item.classList.contains('active')) {
+				item.classList.remove('active');
+				return;
+			} else {
+				accordionTitleAll.forEach(function (i) {
+					i.classList.remove('active');
+				});
+				item.classList.add('active');
+			}
+		});
+	});
+}
+
+accordion('.accordionFaq');
